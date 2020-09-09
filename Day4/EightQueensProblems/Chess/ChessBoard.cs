1c334b77c178e39c29fe34522d909e0d501571de
tango97 <smishra.shubhammishra@gmail.com>
2020-09-10 01:01:43 +0530

    Day4 v0.0

---
+++ Day4/EightQueensProblems/Chess/ChessBoard.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chess
+{
+    class ChessBoard
+    {
+        private int[] Board { get; set; } = new int[8];
+
+        public bool IsSafe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool PlaceQueens(ChessBoard board = null, int column = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool PlaceQueens(List<ChessBoard> solutions, ChessBoard board = null, int column = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Constructors      
+        public ChessBoard()
+        {
+
+        }
+        #endregion
+
+        public override string ToString() => new string(this.Board.Select(n => (char)(n + 48)).ToArray());
+
+        #region All 92 Solutions for 'Eight Queens'
+        #endregion
+
+    }
+}
---
+++ Day4/EightQueensProblems/EightQueens/Tests_Chess.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EightQueens
+{
+    [TestClass]
+    public class Test_Chess
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
